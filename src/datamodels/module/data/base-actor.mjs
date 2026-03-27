e3fea285e73449442c0330ab8e0f8a77349e078d
Zachary Hershowitz <zhershowitz99@gmail.com>
2026-03-26 23:18:12 -0400

    changed it to hit dice
    the way the box is set up made more sense to do hit dice and work on proficiency later

--- src/datamodels/module/data/base-actor.mjs
+++ src/datamodels/module/data/base-actor.mjs
@@ -11,7 +11,7 @@ export default class ForgeActorBase extends ForgeDataModel {
       value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 10 })
     });
-    schema.proficency = new fields.SchemaField({
+    schema.hitdice = new fields.SchemaField({
       value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 5 })
     });
